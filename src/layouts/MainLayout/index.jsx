582110c7a9dd8031a6acb0ca106c3dd3afa9ccf4
Taiwo <taiwoglory136@gmail.com>
2025-04-06 23:01:25 +0100

    navbar change

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/common/Footer";
-import Navbar from "@/components/common/NavBar";
-
+import Navbar from "@/components/common/Navbar";
 const MainLayout = ({ children }) => {
   return (
     <div>
