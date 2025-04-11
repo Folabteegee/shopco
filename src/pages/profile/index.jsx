d5faea69c045a65f3c2df0985f42078a6937c0f1
Taiwo <taiwoglory136@gmail.com>
2025-04-11 18:23:54 +0100

    profile redo

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -23,8 +23,8 @@ export default function Profile() {
   if (!user) return null;
 
   return (
-    <div className=" flex font-satoshi items-center justify-center bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-md w-80 text-center">
+    <div className=" flex font-satoshi mt-20 items-center justify-center ">
+      <div className="  rounded-lg w-80 text-center">
         <h2 className="text-2xl font-bold mb-4">Welcome, {user.firstName}!</h2>
         <p>
           <strong>First Name:</strong> {user.firstName}
@@ -35,12 +35,14 @@ export default function Profile() {
         <p>
           <strong>Email:</strong> {user.email}
         </p>
-        <button
-          onClick={handleLogout}
-          className="mt-4 bg-red-500 text-white px-4 py-2 rounded w-full"
-        >
-          Logout
-        </button>
+        <div className="pt-36">
+          <button
+            onClick={handleLogout}
+            className="mt-4 bg-red-500 text-white px-4 py-2 rounded w-full"
+          >
+            Logout
+          </button>
+        </div>
       </div>
     </div>
   );
